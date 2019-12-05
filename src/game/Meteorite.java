58892b973d84589e432b155333b5f35a1ee637d1
Raffaele Molinari <raffaelemolinari8@gmail.com>
2019-12-05 20:12:46 +0100

    also this

--- src/game/Meteorite.java
+++ src/game/Meteorite.java
@@ -3,34 +3,30 @@ package game;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
-public class Meteorite extends Sprite {
-	Rectangle2D rectangle;
-    JPanel panel;
+public class Meteorite extends AbstractSprite{
+	JPanel panel;
+    int x, y;
+    int angle;
     private AffineTransform transform;
 
-
-    public Meteorite(int x, int y, String path) {
-    	super(x, y, path);
-    	this.transform = new AffineTransform();
-        rectangle = new Rectangle2D.Double(x, y, super.getImage().getWidth(), super.getImage().getHeight());
-    }
-    
-    public void drawMeteorite(Graphics g) {
-   	 Graphics2D g2d = (Graphics2D) g;
-   	 g2d.drawImage(super.getImage(),transform, panel);
-   }
-
-   public void move() {
-	   
-	 int x = this.getX();
-	 int temp_y = this.getY();
-	 this.setY(temp_y+10);
-	 int y = this.getY();
-   	 this.transform.setToTranslation(x,y);
-   	 
-   }
+	 public Meteorite(JPanel panel, int x, int y) {
+		 super(x,y,"../resources/images/fiery-meteorite-icon.png");
+         this.panel = panel;
+         this.angle = 0;
+	 }
+	 
+	 @Override
+	 public void move() {
+		 
+        this.transform.setToTranslation(x,y+=3);           
+        
+	 }
+	 
+	 public void drawAsteroid(Graphics g) {
+      	    Graphics2D g2d = (Graphics2D) g;
+        	g2d.drawImage(super.getImage(), transform, panel);
+      }
 }

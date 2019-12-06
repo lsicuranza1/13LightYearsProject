a92042e23867308b45392c03897fb30dddd03860
Raffaele Molinari <raffaelemolinari8@gmail.com>
2019-12-06 16:07:23 +0100

    temporary change

--- src/game/SpaceShip.java
+++ src/game/SpaceShip.java
@@ -52,6 +52,7 @@ public class SpaceShip extends SpaceshipStructure {
     public void move() {
     	this.setX(this.getX() + dx);
     	this.setY(this.getY() + dy);
+    	//Alessamdro ce la puoi fare
     }
     
     

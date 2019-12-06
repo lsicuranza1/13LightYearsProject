eafeac15db551b7a336a6eb422dbf806f097585a
Raffaele Molinari <raffaelemolinari8@gmail.com>
2019-12-06 15:58:05 +0100

    temporary changes

--- src/game/PanelEsecuzione.java
+++ src/game/PanelEsecuzione.java
@@ -2,36 +2,23 @@ package game;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.util.*;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class PanelEsecuzione extends JPanel implements ActionListener{
-	private String fileNameSpaceShip, fileNameAsteroid, fileNameMeteorite;//, enemyName,fileName2;
-	private SpaceShip spaceShip;
-	private List<Missile> missiles;
-	private List<Asteroid> asteroids;
-    private List<Meteorite> meteorites;
-    private BufferedImage scrollingBackground;
-    private int yOffset = 0;
-    private int yDelta = 1;
-    private static int countToAddAsteroid = 0;
-    private static int countToAddMeteorite = 0;
+	private String fileName;
+	public SpaceShip spaceShip;
     private final int DELAY = 20;
     private Timer timer;
-    
-    private EnemiesSpaceShip enemies;
+    public ObstaclesBackground obs;
 	
 	
 	public PanelEsecuzione() {
@@ -39,53 +26,18 @@ public class PanelEsecuzione extends JPanel implements ActionListener{
 		addKeyListener(new TAdapter());
         setFocusable(false);
         
-        this.fileNameSpaceShip = "../resources/images/navicella-bellissima.png";
-        this.fileNameAsteroid = "../resources/images/asteroid-icon.png";
-        this.fileNameMeteorite = "../resources/images/fiery-meteorite-icon.png";
-        this.spaceShip = new SpaceShip(500,400,fileNameSpaceShip);
-        this.missiles = this.spaceShip.getMissiles();
-        this.asteroids = new ArrayList<Asteroid>();
-        this.meteorites = new ArrayList<Meteorite>();
-        
-        try {
-			this.scrollingBackground=ImageIO.read(getClass().getResource("../resources/images/space.jpg"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        
-        //enemyName = "../resources/images/firstEnemy.png";
-        //enemies = new EnemiesSpaceShip(0,0,enemyName);
+        fileName = "../resources/images/spaceship.png";
+        spaceShip = new SpaceShip(100,100,fileName);
+//        obs = new ObstaclesBackground();
         
         timer = new Timer(DELAY, this);
-        timer.start();                  //creates delay fot the repaint() method
-        
-        
+        timer.start();
 	}
 	
 
 	@Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        if (scrollingBackground != null) {
-            Graphics2D g2d = (Graphics2D) g.create();
-            
-            int xPos = (getWidth() - scrollingBackground.getWidth()) / 2;
-            int yPos = yOffset;
-
-            while (yPos > 0) {
-                yPos -= scrollingBackground.getHeight();
-                g2d.drawImage(scrollingBackground, xPos, yPos, this);
-            }
-
-            yPos = yOffset;
-            while (yPos < getHeight()) {
-                g2d.drawImage(scrollingBackground, xPos, yPos, this);
-                yPos += scrollingBackground.getHeight();
-            }
-
-            g2d.dispose();
-        }
 
         doDrawing(g);
 
@@ -99,6 +51,7 @@ public class PanelEsecuzione extends JPanel implements ActionListener{
         g2d.drawImage(spaceShip.getImage(), spaceShip.getX(),
                 spaceShip.getY(), this);
         
+//        g2d.drawImage()
         
         List<Missile> missiles = spaceShip.getMissiles();
 
@@ -107,24 +60,6 @@ public class PanelEsecuzione extends JPanel implements ActionListener{
             g2d.drawImage(missile.getImage(), missile.getX(),
                     missile.getY(), this);
         }
-        
-        
-        for (Asteroid asteroid : asteroids) {
-            g2d.drawImage(asteroid.getImage(),asteroid.getTransform(), this);
-        }
-        
-        for (Meteorite meteorite : meteorites) {
-        	g2d.drawImage(meteorite.getImage(), meteorite.getTransform(),this);
-        }
-        
-      //g2d.drawImage(enemies.getImage(), enemies.getX(), enemies.getY(), this);//disegna nemico
-        
-        
-//      List<Missile> colpo = enemies.getMissiles();
-//      for(Missile missile : colpo) {
-//      	g2d.drawImage(missile.getImage(), missile.getX(),
-//      			missile.getY(), this);
-//      }
       
     }
     
@@ -132,13 +67,9 @@ public class PanelEsecuzione extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
 		this.updateSpaceShip();
 		this.updateMissiles();
-		this.updateObstacles();
-		this.checkCollisions();
-		yOffset += yDelta;
         this.repaint();
         //System.out.println("ActionPerformed");
     }
-    
     
     private void updateMissiles() {
 
@@ -151,83 +82,17 @@ public class PanelEsecuzione extends JPanel implements ActionListener{
             if (missile.isVisible()) {
             	
                 missile.move();
-                missile.setBounds();
             } else {
 
                 missiles.remove(i);
             }
         }
-        
-//        List<Missile> colpo = enemies.getMissiles();
-//        for(int i = 0; i < colpo.size(); i++) {
-//        	Missile missile = colpo.get(i);
-//        	
-//        	if(missile.isVisible())
-//        		missile.move_colpo();
-//        	else
-//        		colpo.remove(i);
-//        }
-        
-        
     }
     
     public void updateSpaceShip() {
     	spaceShip.move();
-    	spaceShip.setBounds();
-    	//enemies.move(); // movimento nemico
     }
     
-    
-    public void updateObstacles() {
-    	Random random = new Random();
-        
-        int y_asteroid = -1000;
-        int y_meteorite = -1000;
-        int D_W = 1000;
-        int D_H = 600;
-    	
-    	
-    	if (countToAddAsteroid >= 50) {   //maggiore è il valore minore è la frequenza di uscita degli asteroidi (utile per gestione dei livelli)
-            int randX1 = random.nextInt(D_W); //larghezza window
-            asteroids.add(new Asteroid(randX1, y_asteroid, fileNameAsteroid));
-            countToAddAsteroid = 0;
-        }
-        countToAddAsteroid++;
-        
-        if (countToAddMeteorite >= 150) {   //maggiore è il valore minore è la frequenza di uscita degli asteroidi (utile per gestione dei livelli)
-            int randX2 = random.nextInt(D_W);
-            meteorites.add(new Meteorite(randX2, y_meteorite, fileNameMeteorite));
-            countToAddMeteorite = 0;
-        }
-        countToAddMeteorite++;
-        
-        Iterator<Asteroid> it_asteroids = asteroids.iterator();
-
-        while (it_asteroids.hasNext()) {
-            Asteroid asteroid = (Asteroid)it_asteroids.next();
-            if (asteroid.getY() >= D_H || !asteroid.isVisible()) {
-                it_asteroids.remove();
-            } else {
-                 asteroid.move();
-                 asteroid.setBounds();
-            }
-        }
-        
-        
-        Iterator<Meteorite> it_meteorites = meteorites.iterator();
-        
-        while (it_meteorites.hasNext()) {
-            Meteorite meteorite = (Meteorite)it_meteorites.next();
-            if (meteorite.getY() >= D_H || !meteorite.isVisible()) {
-                it_meteorites.remove();
-            } else {
-                 meteorite.move();
-                 meteorite.setBounds();
-            }
-        }
-    }
-    
-   
     
     public class TAdapter extends KeyAdapter{
 	    @Override
@@ -240,63 +105,5 @@ public class PanelEsecuzione extends JPanel implements ActionListener{
 	        spaceShip.keyPressed(e);
 	    }
     }
-    
-    public void checkCollisions() {
 
-        Rectangle2D spaceShipBounds = spaceShip.getBounds();
-        Rectangle2D asteroidBounds;
-        Rectangle2D meteoriteBounds;
-        Rectangle2D missileBounds;
-        
-
-        //Rectangle2D r2 = enemies.getBounds();
-        
-//        Rectangle2D r4 = enemy.getBounds();
-//
-//        System.out.println(r3.intersects(r4));
-//        if (r3.intersects(r4)) {
-//                
-//            spaceShip.setVisible(false);
-//            enemy.setVisible(false);
-//        	//System.out.println("Collisione spaceship-asteroide");
-//            
-//        }
-//
-//        System.out.println(ms.size());
-        
-        for (Meteorite meteorite : meteorites) {
-        	
-        	meteoriteBounds = meteorite.getBounds();
-        	
-        	if(meteoriteBounds.intersects(spaceShipBounds)) {        		
-        		MainFrame.getIstance().updateModalita("game_over");
-        	}
-        	
-        }
-
-        for (Asteroid asteroid : asteroids) {
-        	
-        	asteroidBounds = asteroid.getBounds();
-        	
-        	if (spaceShipBounds.intersects(asteroidBounds)) {            	
-        		MainFrame.getIstance().updateModalita("game_over");            	
-            }
-            
-            for (Missile missile : missiles) {
-            	missileBounds = missile.getBounds();
-            	
-            
-	            if (missileBounds.intersects(asteroidBounds)) {
-	    
-	                missile.setVisible(false);
-	                
-	                asteroid.setVisible(false);
-	                System.out.println("Collisione missile-asteroide");
-	            }
-	            
-	            
-                
-            }
-        }
-    }
 }
--- src/game/patterns/state/StatoInEsecuzione.java
+++ src/game/patterns/state/StatoInEsecuzione.java
@@ -1,43 +1,65 @@
 package game.patterns.state;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
+import java.util.List;
+
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import game.MainFrame;
+import game.Missile;
+import game.ObstaclesBackground;
 import game.PanelEsecuzione;
+import game.SpaceShip;
 
-public class StatoInEsecuzione implements Stato {
 
+public class StatoInEsecuzione extends javax.swing.JFrame implements Stato{
+ 
 	MainFrame mainFrame = MainFrame.getIstance();
+	//private SpaceShip spaceShip = new SpaceShip(100,100,"/13LightYearsProject/src/resources/images/spaceship.png");
 	private PanelEsecuzione panel = new PanelEsecuzione();
-
+	private ObstaclesBackground obs = new ObstaclesBackground();
+    //private SpaceShip ship;
+    
 	public StatoInEsecuzione() {
-		panel.setBackground(Color.BLACK);
-		panel.setSize(1000, 600);
-		mainFrame.getFrame().setVisible(true);
-		mainFrame.getFrame().add(panel);
-		panel.setFocusable(true);
-		panel.requestFocus();
-
-	}
-
-	@Override
+        System.out.println("In esecuzione");
+        
+        //mainFrame.getFrame().setLayout(new BorderLayout());
+        //mainFrame.getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setBackground(Color.BLACK);
+        panel.setSize(1000, 600);
+        //mainFrame.getFrame().add(panel, BorderLayout.CENTER);
+        mainFrame.getFrame().setVisible(true);
+        mainFrame.getFrame().add(panel);
+        //ship = new SpaceShip(300, 300);
+        panel.setFocusable(true);
+        panel.requestFocus();
+        
+    }
+	
+    @Override
 	public void gestioneStato(Modalita modalita, String stato) {
-		if (stato.equals("game_over")) {
-			mainFrame.getFrame().setTitle("GameOver");
-			mainFrame.getFrame().getContentPane().removeAll();
-			mainFrame.getFrame().repaint();
+		// TODO Auto-generated method stub
+		if (stato.equals("pausa"))
+			modalita.setStatoModalita(new StatoPausa());
+		else if(stato.equals("game_over"))
 			modalita.setStatoModalita(new StatoGameOver());
-		} else if (stato.equals("main_menu")) {
-			mainFrame.getFrame().setTitle("Start");
-			mainFrame.getFrame().getContentPane().removeAll();
-			mainFrame.getFrame().repaint();
-			modalita.setStatoModalita(new StatoStart());
-		}else if (stato.equals("restart")) {
-			mainFrame.getFrame().setTitle("Start");
-			mainFrame.getFrame().getContentPane().removeAll();
-			mainFrame.getFrame().repaint();
-			modalita.setStatoModalita(new StatoInEsecuzione());
-		}
 	}
-
+ 
 }

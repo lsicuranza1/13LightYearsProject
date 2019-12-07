3ab08e5b50b3e2e175fc16c572704af6c8d78eab
gwerryz <gerardo.salvati@hotmail.com>
2019-12-07 15:53:20 +0100

    test design pattern State
    test design pattern State

---
+++ src/test/ModalitaTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModalitaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetStatoModalita() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStatoModalita() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/StatoInEsecuzioneTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatoInEsecuzioneTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGestioneStato() {
+		fail("Not yet implemented");
+	}
+
+}

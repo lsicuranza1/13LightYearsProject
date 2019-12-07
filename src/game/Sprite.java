85f0ed160b14b4195be0b4789ffe64fe0ec2d1e4
gwerryz <gerardo.salvati@hotmail.com>
2019-12-07 19:34:39 +0100

    Squashed commit of the following:
    commit b49a4bfdda85368bef33e888cb0ec2621b6adfbd
    Author: gwerryz <gerardo.salvati@hotmail.com>
    Date:   Sat Dec 7 17:48:36 2019 +0100
    
        remove all warning
    
        add task score
    
    commit 2eceb96bec7b7d2c654d50b1255b38b799d37680
    Author: gwerryz <gerardo.salvati@hotmail.com>
    Date:   Sat Dec 7 17:40:28 2019 +0100
    
        Squashed commit of the following:
    
        commit f126d92bfc1ea1c766b6701802678a358c0c88f9
        Author: gwerryz <gerardo.salvati@hotmail.com>
        Date:   Sat Dec 7 17:39:54 2019 +0100
    
            add task score in GameOver
    
            add task score
    
    commit 3ab08e5b50b3e2e175fc16c572704af6c8d78eab
    Author: gwerryz <gerardo.salvati@hotmail.com>
    Date:   Sat Dec 7 15:53:20 2019 +0100
    
        test design pattern State
    
        test design pattern State

--- src/game/Sprite.java
+++ src/game/Sprite.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -15,32 +14,28 @@ public abstract class Sprite {
     private int y;
     private Rectangle2D rectangle;
     private BufferedImage image;
-    private boolean visible = true;
     
 
     public Sprite(int x, int y,  String imageFileName) {
         this.x = x;
         this.y = y;
         this.loadImage(imageFileName);
-        this.rectangle = new Rectangle(x, y, this.getWidth(), this.getHeight());
+        this.setRectangle(new Rectangle2D.Double(x, y, this.getWidth(), this.getHeight()));
     }
     
+    
+    //------Template method---
     private void loadImage(String imageFileName) {
+//        ImageIcon ii = new ImageIcon(imageName);
+//        image =  ii.getImage();
         try {
 			image = ImageIO.read(getClass().getResource(imageFileName));
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.out.println("Image not loaded");
+
 		}
 
-    }
-    
-    public boolean isVisible() {
-        return visible;
-    }
-    
-    public void setVisible(boolean visible) {
-    	this.visible = visible;
     }
 
     public int getX() {
@@ -69,11 +64,17 @@ public abstract class Sprite {
 	public int getHeight() {
 	    return this.image.getHeight(null);
 	}
-	
-	public Rectangle2D getBounds() {
+
+    public abstract void move();
+
+
+	public Rectangle2D getRectangle() {
 		return rectangle;
 	}
 
-    public abstract void move();
+
+	public void setRectangle(Rectangle2D rectangle) {
+		this.rectangle = rectangle;
+	}
     
 }

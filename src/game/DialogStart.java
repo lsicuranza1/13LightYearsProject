98fe512626028e66f15b87c5fec8654c3b1c634d
gwerryz <gerardo.salvati@hotmail.com>
2019-12-14 19:36:31 +0100

    eliminate stato Start and resize of statoInEsecuzione with new controll

--- src/game/DialogStart.java
+++ src/game/DialogStart.java
@@ -1,6 +1,5 @@
 package game;
 
-
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -12,43 +11,61 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 class DialogStart extends JPanel {
-    private static final Color BG = new Color(123, 63, 0);
-    private javax.swing.JButton resume;
-    private javax.swing.JButton restart;
-    private javax.swing.JButton mainMenu;
-    private PanelEsecuzione jPanel;
+	private MainFrame mainframe = MainFrame.getIstance();
+	private static final Color BG = new Color(123, 63, 0);
+	private javax.swing.JButton resume;
+	private javax.swing.JButton restart;
+	private javax.swing.JButton mainMenu;
+	private PanelEsecuzione jPanel;
 
-    //dependency inception
-    public DialogStart(PanelEsecuzione jPanel) {
-    	this.jPanel=jPanel;
-        JLabel pausedLabel = new JLabel("PAUSED");
-        pausedLabel.setForeground(Color.ORANGE);
-        JPanel pausedPanel = new JPanel();
-        pausedPanel.setOpaque(false);
-        pausedPanel.add(pausedLabel);
+	// dependency inception
+	public DialogStart(PanelEsecuzione jPanel) {
+		this.jPanel = jPanel;
+		JLabel pausedLabel = new JLabel("PAUSED");
+		pausedLabel.setForeground(Color.ORANGE);
+		JPanel pausedPanel = new JPanel();
+		pausedPanel.setOpaque(false);
+		pausedPanel.add(pausedLabel);
 
-        setBackground(BG);
-        int eb = 15;
-        setBorder(BorderFactory.createEmptyBorder(eb, eb, eb, eb));
-        setLayout(new GridLayout(0, 1, 10, 10));
-        add(pausedPanel);
-        resume = new JButton("RESUME");
-        restart = new JButton("RESTART");
-        mainMenu = new JButton("MAIN MENU");
-        resume.addActionListener(new java.awt.event.ActionListener() {
+		setBackground(BG);
+		int eb = 15;
+		setBorder(BorderFactory.createEmptyBorder(eb, eb, eb, eb));
+		setLayout(new GridLayout(0, 1, 10, 10));
+		add(pausedPanel);
+		resume = new JButton("RESUME");
+		restart = new JButton("RESTART");
+		mainMenu = new JButton("MAIN MENU");
+		resume.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				resumeActionPerformed(evt);
 			}
 		});
-        add(resume);
-        add(restart);
-        add(mainMenu);
-    }
-    
-    protected void resumeActionPerformed(ActionEvent evt) {
-    	jPanel.setFlagPause(false);
-		
+		restart.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				restartActionPerformed(evt);
+			}
+		});
+		mainMenu.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				mainMenuActionPerformed(evt);
+			}
+		});
+		add(resume);
+		add(restart);
+		add(mainMenu);
 	}
 
-    
+	protected void mainMenuActionPerformed(ActionEvent evt) {
+		mainframe.updateModalita("main_menu");
+	}
+
+	protected void restartActionPerformed(ActionEvent evt) {
+		mainframe.updateModalita("restart");
+	}
+
+	protected void resumeActionPerformed(ActionEvent evt) {
+		jPanel.setFlagPause(false);
+
+	}
+
 }

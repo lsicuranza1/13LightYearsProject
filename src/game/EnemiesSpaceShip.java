2aedd5604f0837b7223bee8d53d9a4a2e7a82273
Salvatore <salvatoreradio@hotmail.it>
2019-12-18 18:20:00 +0100

    Change in enemies class

--- src/game/EnemiesSpaceShip.java
+++ src/game/EnemiesSpaceShip.java
@@ -14,40 +14,39 @@ public class EnemiesSpaceShip extends EnemySpaceshipStructure {
 	private int count = 0; // count the position to reached before exit	(2)
 	
 	private int enemyDelay = 0;  // delay to let enemy move again
-	private int newX_1 = random.nextInt(950);
-	private int newY_1 = random.nextInt(200);
+	private int newX = random.nextInt(950);
+	private int newY = random.nextInt(200);
 	
 	
 	public EnemiesSpaceShip(int x, int y, String path) {
 		super(x, y, path);
 	}
 
-
 	public void move() {
 	
 		
-		if(this.getX()>newX_1) {
+		if(this.getX()>this.newX) {
 			if(this.count==2)
 				this.setX(this.getX());
 			else
 				this.setX(this.getX()-5);	
 		}
 		
-		if(this.getX()<newX_1) {
+		if(this.getX()<this.newX) {
 			if(this.count==2) 
 				this.setX(this.getX());
 			else	
 				this.setX(this.getX()+5);
 		}
 		
-		if(this.getY()<=newY_1) {
+		if(this.getY()<=this.newY) {
 			if(this.count==2) 
 				this.setY(this.getY()+5);
 			else	
 				this.setY(this.getY()+5);
 		}
 		
-		if(this.getY()>=newY_1) {
+		if(this.getY()>=this.newY) {
 			if(this.count==2) 
 				this.setY(this.getY()+5);
 			else	
@@ -57,18 +56,15 @@ public class EnemiesSpaceShip extends EnemySpaceshipStructure {
 		this.enemyDelay++;
 		
 		if(this.enemyDelay==150 && this.count <2) {
-				newX_1 = random.nextInt(950);
-				newY_1 = random.nextInt(200);
-				this.count++;
-				this.enemyDelay=0;
+				this.setnewX(random.nextInt(950));
+				this.setnewY(random.nextInt(200));
+				this.setCount(this.getCount()+1);
+				this.setEnemyDelay(0);
 			
 		}
 		
-
-
 	}
 	
-
 	public void fire() {
 		int x = this.getX();
 		int y = this.getY();
@@ -90,7 +86,47 @@ public class EnemiesSpaceShip extends EnemySpaceshipStructure {
 		}.start();
 	}
 	
-	
+	public int getCount() {
+		return count;
+	}
+
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+	public int getEnemyDelay() {
+		return enemyDelay;
+	}
+
+
+	public void setEnemyDelay(int enemyDelay) {
+		this.enemyDelay = enemyDelay;
+	}
+
+
+	public int getnewX() {
+		return newX;
+	}
+
+
+	public void setnewX(int newX) {
+		this.newX = newX;
+	}
+
+
+	public int getnewY() {
+		return newY;
+	}
+
+
+	public void setnewY(int newY) {
+		this.newY = newY;
+	}
+
+
+
 
 
 	
---
+++ src/test/EnemiesSpaceShipTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EnemiesSpaceShipTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFire() {
+		fail("Not yet implemented");
+	}
+
+}
